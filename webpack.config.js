693e8089c197b87e9bae7385bf2f7290ca791669
yoichi kobayashi <info@tplh.net>
2018-08-25 01:38:12 +0900

    added modules to add autoprefixer to CSS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const ExtractText = require('extract-text-webpack-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
 
 const extractSCSS = new ExtractText('css/main.css');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/main.js',
+  entry: [
+    './src/js/main.js',
+    './src/css/main.scss',
+  ],
   output: {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dst')
@@ -16,9 +20,21 @@ module.exports = {
         test: /\.scss$/,
         use: extractSCSS.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [
+                  postcssPresetEnv()
+                ]
+              }
+            },
+            'sass-loader'
+          ]
         })
-      }
+      },
     ]
   },
   plugins: [
