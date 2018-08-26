824b7d4b2c44134564b3faf3fa649c4e21131344
yoichi kobayashi <info@tplh.net>
2018-08-26 16:36:05 +0900

    added a process to run imagemin.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ const glob = require('glob');
 const path = require('path');
 const postcssPresetEnv = require('postcss-preset-env');
 
-const entries = glob.sync('./src/**/*.pug');
+const entries = [
+    './src/js/main.js',
+    './src/css/main.scss',
+  ].concat(
+    glob.sync('./src/**/*.pug'),
+    glob.sync('./src/img/**/*.*')
+  );
 
 module.exports = {
   mode: 'development',
-  entry: [
-    './src/js/main.js',
-    './src/css/main.scss',
-  ].concat(entries),
+  entry: entries,
   output: {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dst')
@@ -62,6 +65,36 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               presets: ['env'],
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: 'src'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 90
+              },
+              pngquant: {
+                quality: '80-90',
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 90
+              }
             }
           }
         ]
